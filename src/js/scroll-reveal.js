4eef8cf31bebc5cf6fa0cb2fadaf6b4513390bd3
Leonardo_zyh <956731563@qq.com>
2018-11-20 22:45:02 +0800

    scrollreveal

---
+++ src/js/scroll-reveal.js
@@ -0,0 +1,28 @@
+
+    // import ScrollReveal from 'scrollreveal';
+
+    window.sr = new ScrollReveal();
+    sr.reveal('.headline', {
+        // 动画的时长
+        // duration: 1000,
+        // 延迟时间
+        delay: 100,
+        // 动画开始的位置，'bottom', 'left', 'top', 'right'
+        origin: 'top',
+        // 回滚的时候是否再次触发动画
+        reset: false,
+        // 在移动端是否使用动画
+        mobile: true,
+        // 滚动的距离，单位可以用%，rem等
+        distance: '100px',
+        // 其他可用的动画效果
+        opacity: 0.001,
+        easing: 'linear',
+        scale: 0.9,
+      });
+  
+
+
+    
+
+
